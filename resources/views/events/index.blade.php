a5ae97d4c9885854fee7a8fbc41ff452e11bb9a6
Patrick Rose <pjr0911025@googlemail.com>
2015-10-12 23:36:46 +0100

    Update the events page to use theme

--- resources/views/events/index.blade.php
+++ resources/views/events/index.blade.php
@@ -4,43 +4,53 @@
     Events
 @endsection
 
+@section('page-header')
+    <div id="blue">
+        <div class="container">
+            <div class="row">
+                <h3>Events</h3>
+            </div>
+        </div>
+    </div>
+@endsection
+
 @section('content')
 
-    <h1 class="page-header">
-        BoardSoc Events
-    </h1>
-
-    @foreach($events->chunk(3) as $row)
+    <div class="container mtb">
 
         <div class="row">
-            @foreach($row as $event)
-                <div class="col-md-4">
-                    <h2 class="page-header">
+            <div class="col-lg-8 col-lg-offset-2">
+                @forelse($events->chunk(3) as $row)
+                    @foreach($row as $event)
+                        <h3 class="ctitle">
+                            <a href="{{ route('events.show', ['event'=> $event]) }}">
+                                {{ $event->name }}
+                            </a>
+                            <span class="csmall">
+                                {{ $event->date->format('l jS M') }}
+                            </span>
+                        </h3>
+
+                        <p>
+                            {{ $event->getFirstParagraph() }}
+                        </p>
                         {!!
-                        link_to_route(
-                        'events.show',
-                        $event->name,
-                        ['event'=> $event]
-                        )
+                            link_to_route(
+                                'events.show',
+                                'More information',
+                                ['event'=> $event]
+                            )
                         !!}
-                        <small>
-                            {{ $event->date->format('l jS M') }}
-                        </small>
-                    </h2>
-
-                    <p>
-                        {{ $event->getFirstParagraph() }}
-                    </p>
-                    {!!
-                    link_to_route(
-                    'events.show',
-                    'More information',
-                    ['event'=> $event]
-                    )
-                    !!}
-                </div>
-            @endforeach
+ 		        <div class="hline"></div>
+		 	
+		 	<div class="spacing"></div>
+		    @endforeach
+                @empty
+                    <div class="alert alert-info">
+                        No events organised yet, check back soon!
+                    </div>
+                @endforelse
+            </div>
         </div>
-
-    @endforeach
+    </div>
 @endsection
