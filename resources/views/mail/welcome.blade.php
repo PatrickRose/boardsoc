b8e594ffbfa468090455d1896662e38218652df2
Patrick Rose <pjr0911025@googlemail.com>
2017-09-07 22:27:13 +0100

    Don't hardcode route to main page

--- resources/views/mail/welcome.blade.php
+++ resources/views/mail/welcome.blade.php
@@ -1,7 +1,7 @@
 Hi there!
 
 We've just got you signed up for the website
-(http://boardsoc.patrickrosemusic.co.uk). Wooooo!
+({{ route('home') }}). Wooooo!
 
 Your password is "{{$password}}" - don't forget this! - and you can use the
 email address you signed up with ({{$email}}) to login!
