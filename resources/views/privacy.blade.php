395b4b6b10a57a7e5e9e01c07b79dcaa9ec86e2f
Patrick Rose <pjr0911025@googlemail.com>
2018-05-22 20:33:10 +0100

    Update privacy policy so it's more GDPR compliant

--- resources/views/privacy.blade.php
+++ resources/views/privacy.blade.php
@@ -25,20 +25,79 @@
 				</p>
 
 				<p>
-					Members are entitled to an account on this website to facilitate loaning of games and notifications of upcoming events. This will usually be done by a committee member at the beginning of the year in bulk or when someone signs up at an event.
+					We take your privacy policy seriously. We are a controller of the personal information that you provide to us and this privacy notice sets out how, why and for how long we will use your personal data, as well as who it is shared with. It also explains your legal rights as a "data subject" and how to exercice them
 				</p>
+
+				<h2>
+					What we need from you
+				</h2>
+
+				<p>
+					We may may ask for you personal information to create an account on this website to facilitate loaning of games and notifications of upcoming events. This will usually be done by a committee member at the beginning of the year in bulk or when someone signs up at an event.
+				</p>
+
+				<h2>
+					Why we need your personal inforamation - lawful basis: consent
+				</h2>
+
+				<p>
+					If you become a member, you agree to be contacted by us. We will use this only for the following purposes:
+
+					<ul>
+						<li>To inform you about upcoming events</li>
+					</ul>
+
+					We <em>may</em> in future add further functionality to the website for other features.
 
 				<p>
 					When your account is created you will not receive notifications by default - you'll have to explicitly opt in to this.
 				</p>
 
-				<p>
-					We only store your name and your email to provide this. If you don't want us to even have this, then log in and delete your account - it'll be deleted straight away. If you have any issues, then contact <a href="mailto:boardsoc@sheffield.ac.uk">boardsoc@sheffield.ac.uk</a>.
-				</p>
+				<h2>
+					Who we share your personal information with:
+				</h2>
 
 				<p>
-					Not having an account means you can't loan a game from the society - sorry, but it's the easiest way of us tracking who's got which game.
+					Nobody :-)
 				</p>
+
+				<h2>
+					Third party suppliers with access to personal data
+				</h2>
+
+				<p>
+					BoardSoc may use third party suppliers to provide services. These suppliers may process personal data on our behalf as “processors” and are subject to contractual conditions to only process that personal information under our instructions, and to protect it.
+				</p>
+
+				<ul>
+					<li>Our website is hosted by Host Media - you can read their <a href="https://www.hostmedia.co.uk/privacy-policy/">privacy policy here</a></li>
+				</ul>
+
+				<h2>
+					How we protect your personal information
+				</h2>
+
+				<ul>
+					<li>Electronic data is stored using third party suppliers as listed above.</li>
+					<li>Poynton Ceilidhs’ website is hosted on a server in the EU</li>
+				</ul>
+
+				<h2>
+					How long we keep your personal information
+				</h2>
+
+				<p>
+					We keep details on this website for the duration of the academic year. All accounts are deleted between July and September each year. You can also delete your account yourself if you want it gone earlier.
+				</p>
+
+				<h2>You have a right to:</h2>
+				<ul>
+					<li>Change your communication preferences or restrict the processing of your personal data for specific purposes.</li>
+					<li>Request that we correct your personal data if you believe it is inaccurate or incomplete.</li>
+					<li>Request that we delete your personal information.</li>
+					<li>Access the personal data that we hold about you through a ‘subject access request’ by emailing us at <a href="mailto:boardsoc@sheffield.ac.uk">boardsoc@sheffield.ac.uk</a></li>
+
+				</ul>
 
 				<p>
 					If you have online membership via the Student's Union site, then <a href="mailto:boardsoc@sheffield.ac.uk">get in touch with us</a> about removing it from there.
